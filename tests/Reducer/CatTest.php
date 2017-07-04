be3b958ac7abd7ab6daec8b507755ecfb4fa6d7f
Julien VITTE <julien.vitte@insidegroup.fr>
2017-07-04 22:40:47 +0200

    Adds miising test for concatenate lists with empty items

--- tests/Reducer/CatTest.php
+++ tests/Reducer/CatTest.php
@@ -15,4 +15,12 @@ class CatTest extends TestCase
 
         self::assertEquals([0, 1, 2, 3, [4, 5], 6], $concatenated);
     }
+
+    public function test_removes_empty_nested_lists()
+    {
+        $concatenated = (new Transformer([0 ,1, [], [2, 3], [[4, 5], 6]]))
+            ->cat()->toArray();
+
+        self::assertEquals([0, 1, 2, 3, [4, 5], 6], $concatenated);
+    }
 }
